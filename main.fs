0587665ba313e7b6b24fa4f309f7c637ac3edc1e
Thomas <thomas@makerspace-rheinfelden.ch>
2016-12-27 03:13:35 +0100

    Switched to 1/10K internal temperature representation

--- main.fs
+++ main.fs
@@ -5,6 +5,21 @@ include pid.fs
 include tempfix.fs
 
 
+\ Stops the heating
+: stop-heating ( -- )
+ 0 manual
+;
+
+\ Internal temperature representation to real temperature (rounded!)
+: int2temp ( s -- s )
+  10 /
+;
+
+\ Real temperature to internal representation
+: temp2int ( s -- s )
+  10 *
+;
+
 \ Init pwm for output
 PB6 constant PWM-OUT
 120 PWM-OUT pwm-init
@@ -32,42 +47,41 @@ IMODE-ADC ADC-IN io-mode!
 
 \ Measures adc input and checks for some common error conditions
 \ Switches to manual mode if there is an errror
-\ Result is in 1/5K (diff to environment)
+\ Result is in 1/10K (diff to environment)
 : measure ( -- s_temp )
   ADC-IN adc ADC-IN adc + 2/
   dup notip? IF
     ." No tip connected - stopping pid"
-    0 manual
+    stop-heating
     drop -1
   ELSE
-    \ y=0.1098x+29.471 (*5 as we're working with 1/5K)
-    s2f 0,549 f* 147,355 d+ f2s
+    \ y=0.1098x+29.471 (*5 as we're working with 1/10K)
+    s2f 1,098 f* 294,71 d+ f2s
   THEN
 ;
 
 
 \ Check if the temp goes up while heating
-\ Sets an error if one sec passes without temp increase
 0 variable heat-start
 0 variable heat-temp
 0 variable heat-state
 
-: heatermonitor-checkifok ( pwm -- pwm)
+: heatmon-checkifok ( pwm -- pwm)
   0 heat-state !
   last-input @ heat-temp @ - 50 < IF
-    \ less than 10K? There has to be an error
+    \ less than 5K? There has to be an error
     ." Heatermonitor decided there is something wrong as temp is NOT rising - stopping"
-    ." Temp at start:" heat-temp @ 5 / . ." now:" last-input @ 5 / .
-    0 manual \ disable pid
+    ." Temp at start:" heat-temp @ int2temp . ." now:" last-input @ int2temp .
+    stop-heating
     drop 0   \ force pwm out to 0
   ELSE
     ." heatmon is happy - stopped"
   THEN
 ;
 
-: heatermonitor-on ( pwm -- pwm )
-  millis heat-start @ - 1000 > IF \ at least 1sec passed
-    heatermonitor-checkifok
+: heatmon-ison ( pwm -- pwm )
+  millis heat-start @ - 2000 > IF \ at least 2sec passed
+    heatmon-checkifok
   ELSE \ check if we're still needed here
     dup 6000 < IF \ pwm < 6000, disable myself
       0 heat-state !
@@ -76,7 +90,7 @@ IMODE-ADC ADC-IN io-mode!
   THEN
 ;
 
-: heatermonitor-off ( pwm -- pwm )
+: heatmon-isoff ( pwm -- pwm )
   dup 8000 > IF \ check if PWM > 8000
     1 heat-state !
     millis heat-start !
@@ -85,29 +99,29 @@ IMODE-ADC ADC-IN io-mode!
   THEN
 ;
 
-: heatermonitor ( pwm -- pwm )
+: heatmon ( pwm -- pwm )
   heat-state @ case
   0 of \ standby or not initialized
-    heatermonitor-off
+    heatmon-isoff
     endof
   1 of \ temp *should* go up
-    heatermonitor-on
+    heatmon-ison
     endof
   endcase
 ;
 
+: output-is ( -- )
+  ." TEMP:"
+  last-input @ int2temp .
+;
+
+
 \ Control loop
 
-\ what happens when 
-\  ms action (next-step #)
-\   0 disable pwm (0)
-\   2 unprotect opamp (1)
-\   9 measure adc value (2)
-\     protect opamp, pid calculation
-\  10 enable pwm with new value (3)
-\ 100 repeat
 0 variable next-step
 0 variable wait-time
+
+: next ( step wait -- ) 1 - wait-time ! next-step !  ;
 
 : 1mshandler ( -- )
   wait-time @ 0<> IF
@@ -116,15 +130,16 @@ IMODE-ADC ADC-IN io-mode!
     next-step @ case
     0 of \ == 0 -> disable pwm
       0 PWM-OUT pwm
-      1 next-step ! 1 wait-time ! \ 2 ms
+      1 2 next \ next step in 2 ms
       endof
     1 of \ == 1 -> unprotect opamp
       opamp-unprot
-      2 next-step ! 7 wait-time ! \ 8 ms
+      2 8 next \ next step in 9 ms
       endof
     2 of \ == 2 -> do the work (measure, protect opamp, calculate pid)
-      measure opamp-prot pid heatermonitor PWM-OUT pwm
-      0 next-step ! 89 wait-time ! \ 90 ms
+      measure opamp-prot pid heatmon PWM-OUT pwm
+      output-is
+      0 90 next \ next step in 90 ms
       endof
     endcase
   THEN
@@ -142,6 +157,6 @@ IMODE-ADC ADC-IN io-mode!
 
 
 \ Init PID
-64,0 0,3 0,0075 100 10000 pid-init
-1550 set \ about 330°C @20°C environment
+120,0 1,5 0,0075 100 10000 pid-init
+3100 set \ about 330°C @20°C environment
 enable-systick-pid
